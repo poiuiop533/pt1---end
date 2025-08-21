a06efd96bcd415ca181244bb197c54248f1c0ce1
poiuiop533 <lyxue_910@hotmail.com>
2025-08-21 09:58:47 +0800

    Add files via upload

--- still_alive_credit.py
+++ still_alive_credit.py
@@ -1,31 +1,3 @@
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#
-# The copyright for song <Still Alive> belongs to Jonathan Coulton and Valve Software
-# Contact me if there's copyright violation and if deletion of the content is needed.
-#
-#
-#
-# PORTAL--STILL ALIVE demo by LHF (BD4SUP)
-# September 2021
-# Runs with Python 3 on most terminals with an 80x24 characters area.
-# This program was adjusted specifically for serial terminal under Linux
-# (in this case, an Informer 213 @ 19200bps and Ubuntu 14.04)
-# Feel free to do whatever you like with this code.
-# Personnel credits come from this project: https://github.com/xBytez/aperturescience
-# ASCII arts come from this project: https://sites.google.com/site/gaddc11/Stillalive.rar?attredirects=0&d=1
-# My blog: EE Archeology @ http://7400.me
-
 import pygame
 #import playsound
 import time
